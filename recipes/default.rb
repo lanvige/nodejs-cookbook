80c480b9a27d3f505e9c68bbe6287123c61863b1
Lanvige Jiang <lanvige@gmail.com>
2014-12-11 16:58:10 +0800

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,34 @@
-package 'python-software-properties'
-# package ubuntu >= 12.10
-# package 'python-software-common'
+#
+# Cookbook Name:: nodejs
+# Recipe:: default
+#
+# Copyright 2014, Lanvige Jiang
+#
+# All rights reserved - Do Not Redistribute
+#
 
-apt_repository("node.js") do
-  uri "http://ppa.launchpad.net/chris-lea/node.js/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "C7917B12"
+bash 'adding stable nodejs ppa' do
+  user 'root'
+  code <<-EOC
+    add-apt-repository ppa:chris-lea/node.js
+    apt-get update
+  EOC
 end
-package 'nodejs'
+
+# install nginx
+package "nodejs"
+
+
+# package 'python-software-properties'
+# # package ubuntu >= 12.10
+# # package 'python-software-common'
+
+# apt_repository("node.js") do
+#   uri "http://ppa.launchpad.net/chris-lea/node.js/ubuntu"
+#   distribution node['lsb']['codename']
+#   components ["main"]
+#   keyserver "keyserver.ubuntu.com"
+#   key "C7917B12"
+# end
+# package 'nodejs'
+
